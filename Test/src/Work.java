7cad45c53baa09bcc65af4b6ea10bcdc98dd1678
Artem Novik <timy2517@gmail.com>
2016-02-12 13:35:20 -0300

    11

--- Test/src/Work.java
+++ Test/src/Work.java
@@ -2,23 +2,12 @@
 public class Work {
 	public static void main(String[] args){
 		
-		Car car1 = new Car("AB23gd", "Audi", 2010);
-		car1.myStatic = 5;
+		Transport car;
 		
-		Car car2 = new Car("AB23gdd", "BMW", 2011);
-		car2.myStatic = 7;
-
+		car = new Car("AB23gd", "Audi", 2010);
+		car.myStatic = 5;
 		
-		Poezd po = new Poezd("234", 1996);
-
-		
-		Airplane airp = new Airplane("d747", 2001);
-		
-		System.out.println(car1.toString());
-		System.out.println(po.toString());
-		System.out.println(airp.toString());
-		System.out.println(car1.myStatic);
-		System.out.println(car2.myStatic);
+		car.buildEngine();
 	}
 
 }
