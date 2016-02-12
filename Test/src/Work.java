d8b16993aeaccce637239e99bb1062446f026e3e
Artem Novik <timy2517@gmail.com>
2016-02-12 13:41:14 -0300

    11

--- Test/src/Work.java
+++ Test/src/Work.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
 
 public class Work {
-	public static void main(String[] args){
-		
-		Transport car;
-		
-		car = new Car("AB23gd", "Audi", 2010);
-		car.myStatic = 5;
-		
-		car.buildEngine();
+	public static void main(String[] args) {
+
+		Transport transport;
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+
+		if (a == 0) {
+			transport = new Car("AB23gd", "Audi", 2010);
+		} else if (a == 1) {
+			transport = new Poezd("AB23gd", 2010);
+		} else
+			transport = new Airplane("AB23gd", 2010);
+
+		// car = new Car("AB23gd", "Audi", 2010);
+		// car.myStatic = 5;
+
+		transport.buildEngine();
 	}
 
 }
