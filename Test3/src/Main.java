58683eeca3606979714d0eef7a115879a51fcb82
Artem Novik <timy2517@gmail.com>
2016-02-19 14:52:36 -0300

    date

--- Test3/src/Main.java
+++ Test3/src/Main.java
@@ -6,18 +6,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String str = "+375 297 4-502 69";
-		
-		str.replaceAll(" ", "d");
+		String str = "+3775 29d7 4-502 69";
+
+		str = str.replaceAll("\\D", "");
+
 
 		//шаблон для регулярного выражения по которому будет поиск
 		//Pattern pattern = Pattern.compile("^[A-Za-z]+#+[0-9]+$");
 		//Pattern pattern = Pattern.compile("^\\D+\\#\\d+$");
 		//Pattern pattern = Pattern.compile("^\\d{4}+\\s+[A-Q]{2}+[-]+\\d+$");
-		Pattern pattern = Pattern.compile("^\\+\\d{1}");
+		Pattern pattern = Pattern.compile("^\\d{12}$");
 		
 		//Matcher - объект который умеет находить и сравнивать по заданному шаблону (паттерну)
-		Matcher matcher = pattern.matcher(str);
+		Matcher matcher = pattern.matcher(str); //посмотреть групп
 		
 		System.out.println(str);
 		
---
+++ Test3/src/test/test/Main.java
@@ -0,0 +1,91 @@
+package test.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		Date date = new Date();
+		System.out.println(date);
+		System.out.println(date.getTime());
+		
+		
+		SimpleDateFormat format = new SimpleDateFormat("MMMM yyyy z H:mm a");
+		String textDate = format.format(date);
+		
+		System.out.println(textDate);
+		
+		String s1;
+		
+		switch(date.getDate()%10){
+		case 1:
+			s1 = "st";
+			break;
+		case 2:
+			s1 = "nd";
+			break;
+		case 3:
+			s1 = "rd";
+			break;
+			
+			default:
+				s1 = "th";
+				
+		}
+		
+		System.currentTimeMillis(); //время в милисекундах с 01.01.1970
+		
+		System.out.println(date.getDate() + s1 + " " + textDate);
+		
+		Date myDate = null;
+		
+		try{
+			myDate = format.parse("2015-09-06");
+		}catch(ParseException e){
+			System.out.println("формат неверный");
+		}
+		System.out.println(myDate);
+		
+		
+		GregorianCalendar calendar = new GregorianCalendar();
+		
+		Date dd = calendar.getTime();
+		long mil = calendar.getTimeInMillis();
+		
+		int day = calendar.get(GregorianCalendar.DATE);
+		System.out.println(day);
+		
+		int mon = calendar.get(GregorianCalendar.MONTH);
+		System.out.println(mon+1);
+		
+
+		calendar.set(GregorianCalendar.MONTH, 2);
+		
+		System.out.println(calendar.getTime());
+		
+		calendar.add(GregorianCalendar.DATE, -50);
+		
+		System.out.println(calendar.getTime());
+		
+		
+		GregorianCalendar calendar1 = new GregorianCalendar();
+		GregorianCalendar calendar2 = new GregorianCalendar();
+		
+		calendar2.set(GregorianCalendar.MONTH, 5);
+		calendar2.set(GregorianCalendar.DATE, 29);
+		
+		int m = calendar2.get(GregorianCalendar.MONTH)-calendar1.get(GregorianCalendar.MONTH);
+		int d = calendar2.get(GregorianCalendar.DATE)-calendar1.get(GregorianCalendar.DATE);
+		
+		System.out.println(m + " мес " + d + " дней");
+		
+		
+		
+
+	}
+}
