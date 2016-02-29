d52dc5c81989a9378658017774517eaea1739d1d
timy2517 <timy2517@gmail.com>
2016-02-29 18:09:46 +0300

    not ready

---
+++ HomeWork12/src/art/home/work/Human.java
@@ -0,0 +1,28 @@
+package art.home.work;
+
+public class Human {
+	
+	private String name;
+	private String surname;
+	private String patronymic;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getPatronymic() {
+		return patronymic;
+	}
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+
+}
