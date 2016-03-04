96874f3f65b5585b85692f210feee08f36566f90
timy2517 <timy2517@gmail.com>
2016-03-04 23:05:31 +0300

    11

--- Lessons/src/work15/Main.java
+++ Lessons/src/work15/Main.java
@@ -1,6 +1,8 @@
 package work15;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Main {
@@ -8,16 +10,36 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*long timeStart = System.nanoTime();
-
+		
+		long timeStart = System.nanoTime();
+		
 		int[] mas = {1, 2, 3, 4, 5, 5, 6, 4, 3, 2, 1};
 		
-		int x = 0;
-		Arrays.sort(mas);
+		//Map < Integer , Integer > map = new HashMap < Integer, Integer> () ;
+		
+		
+		
+		////////////////s//////////////
+		//Arrays.sort(mas);
 
-		int x = -1;
+		int x = 0;
 
 		for (int i = 0; i < mas.length; i++) {
+			
+			x = x^mas[i];
+			/*int x = 0;
+			for (int i = 0; i < mas.length; i++) {
+				x = x^mas[i];
+			}*/
+			/*if (map.containsKey(mas[i]))
+			{
+				map.remove(mas[i]);
+			}else
+			{
+				map.put(mas[i],1);
+			}*/
+			/*
+			
 			if (i != mas.length - 1) {
 				if (mas[i] != mas[i + 1]) {
 					x = mas[i];
@@ -25,17 +47,18 @@ public class Main {
 				}
 				i++;
 			} else
-				x = mas[mas.length - 1];
+				x = mas[mas.length - 1];*/
 		}
 		
-		for (int i = 0; i < mas.length; i++) {
-			x = x^mas[i];
-		}
+		////////////////////////////////
+		long stop=System.nanoTime() - timeStart;
 		
+		//System.out.println(map.toString());
+		//System.out.println(stop);
 		System.out.println(x);
-		System.out.println(System.nanoTime() - timeStart);*/
+		System.out.println(stop);
 		
-		Random rand = new Random();
+		/*Random rand = new Random();
 		int c = rand.nextInt(899) + 100;
 		//int c = 666;
 		
@@ -49,7 +72,9 @@ public class Main {
 		
 		if((c1 == c2 && c2 == c3) || (c1 == c2-1 && c2 == c3-1)){
 			System.out.println("happy");
-		}
+		}*/
+		
+		
 
 	}
 
