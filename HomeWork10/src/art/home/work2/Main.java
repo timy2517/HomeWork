363400fbde98fed28e8c1490da1d9ef2eafd87d8
timy2517 <timy2517@gmail.com>
2016-03-07 21:30:26 +0300

    ready

--- HomeWork10/src/art/home/work2/Main.java
+++ HomeWork10/src/art/home/work2/Main.java
@@ -1,9 +1,19 @@
 package art.home.work2;
 
+import art.home.work2.Sity.Area;
+import art.home.work2.Sity.Square;
+import art.home.work2.Sity.Street;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Sity minsk = new Sity("Minsk");
+		
+		Street nezavisimostyStr = minsk.new Street("Nezavisimosty");
+		Area leninaPl = minsk.new Area("Lenina");
+		Square kievskijSkv = minsk.new Square("Kievskij");
 
 	}
 
---
+++ HomeWork10/src/art/home/work2/Sity.java
@@ -0,0 +1,36 @@
+package art.home.work2;
+
+public class Sity {
+	
+	String sityName;
+	
+	public Sity(String sityName){
+		this.sityName = sityName;
+	}
+	
+	
+	class Street{
+		String streetName;
+		
+		public Street(String streetName){
+			this.streetName = streetName;
+		}
+	}
+	
+	class Area{
+		String AreaName;
+		
+		public Area(String AreaName){
+			this.AreaName = AreaName;
+		}
+	}
+	
+	class Square{
+		String squareName;
+		
+		public Square(String squareName){
+			this.squareName = squareName;
+		}
+	}
+
+}
