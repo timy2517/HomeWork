507af12e68b7c0d06c3d87bad75bbcd70cf5b2ce
timy2517 <timy2517@gmail.com>
2016-03-21 02:54:16 +0300

    in work

--- FinalProject/src/art/home/work/GSONParser.java
+++ FinalProject/src/art/home/work/GSONParser.java
@@ -4,13 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class GSONParser {
 	public Root parsing() { //метод возвращает root с данными из json	
 		Root root = null;
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("shop.json"));
-			Gson gson = new Gson();
+			
+			GsonBuilder gsonBuilder = new GsonBuilder();
+			gsonBuilder.setDateFormat("yyyy-MM-dd");
+			Gson gson = gsonBuilder.create();
 			root = gson.fromJson(reader, Root.class); // конвертируем json в root
 		} catch (Exception e) {
 			System.out.println("GSON Error = " + e.getMessage());
--- FinalProject/src/art/home/work/JecksonParser.java
+++ FinalProject/src/art/home/work/JecksonParser.java
@@ -1,14 +1,20 @@
 package art.home.work;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JecksonParser {
-	public Root parsing() { //метод возвращает root с данными из json	
+	public Root parsing() { // метод возвращает root с данными из json
 		Root root = null;
 		try {
 			File jsonFile = new File("shop.json");
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			ObjectMapper mapper = new ObjectMapper();
+
+			mapper.setDateFormat(dateFormat);
 			root = mapper.readValue(jsonFile, Root.class); // конвертируем json в root
 		} catch (Exception e) {
 			System.out.println("JECKSON Error = " + e.getMessage());
